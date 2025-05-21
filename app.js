4ec7e232bbdd27c47caae74e625924b6392beb29
Prabhdeep035 <prabhdeep9953@gmail.com>
2025-05-21 12:26:49 +0530

    added loader

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 const but=document.querySelector("#but");
+const loader=document.getElementById("loader");
 // http://www.omdbapi.com/?i=tt3896198&apikey=9d7fc2f6
 async function searchMovie(){
     const key="9d7fc2f6";
     const movie=document.getElementById("moviename").value;
     const url=`https://www.omdbapi.com/?t=${encodeURIComponent(movie)}&apikey=${key}`;
+    loader.style.display = "flex";
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
 
-    const res = await fetch(url);
-    const data = await res.json();
+         const container=document.getElementById("content");
 
-    const container=document.getElementById("content");
-
-    if(data.Response=="True"){
-        container.innerHTML=`
-        <img src="${data.Poster}" alt="Movie Poster">
-        <h2>${data.Title} (${data.Year})</h2>
-        <p><strong>IMDB Rating:</strong> ${data.imdbRating}</p>
-        <p><strong>Genre:</strong> ${data.Genre}</p>
-        <p><strong>Plot:</strong> ${data.Plot}</p>`;
+        if(data.Response=="True"){
+            container.innerHTML=`
+            <img src="${data.Poster}" alt="Movie Poster">
+            <h2>${data.Title} (${data.Year})</h2>
+            <p><strong>IMDB Rating:</strong> ${data.imdbRating}</p>
+            <p><strong>Genre:</strong> ${data.Genre}</p>
+            <p><strong>Plot:</strong> ${data.Plot}</p>`;
+        }
+        else{
+            container.innerHTML='<p class="error">❌ Movie not found.</p>';
+        }
     }
-    else{
-        container.innerHTML='<p class="error">❌ Movie not found.</p>';
+    catch(error){
+        console.error("Error Fetching:",error);
+    }
+    finally{
+        // loader.style.display = "none";
     }
 }
 
